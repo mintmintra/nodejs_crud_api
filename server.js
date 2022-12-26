b60ef3d492d897359dd2dc56265222b2d6755753
mintmintra <mintracss019@gmail.com>
2022-12-27 01:02:23 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const _ = require('lodash');
+
+const server = app.listen(3000, () => {
+    console.log('Nodejs is running on port 3000!')
+})
+
+module.exports = app;
