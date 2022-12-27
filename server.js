ab40ebf307fe48e6a3be2f29fd44e6f3a3b44bb9
mintmintra <mintracss019@gmail.com>
2022-12-27 23:46:33 +0700

    server.js

--- server.js
+++ server.js
@@ -7,4 +7,16 @@ const server = app.listen(3000, () => {
     console.log('Nodejs is running on port 3000!')
 })
 
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "movie"
+});
+
+db.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
 module.exports = app;
